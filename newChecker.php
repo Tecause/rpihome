68a3c9481fb61677ecb00634ee78d5cc8e6a516e
Luminous13 <neoval100@gmail.com>
2018-10-16 01:21:03 +0800

    New Scheduler

--- newChecker.php
+++ newChecker.php
@@ -3,15 +3,19 @@
 	include './db.php';
 
 	if (isset($_POST)) {
-		$sql = "SELECT templateName FROM schedules WHERE `scheduleDay` LIKE '%".date('l')."%' AND TIME(`timeStart`) <= '".date('H:m:s')."' AND DATE_ADD(TIME(`timeStart`), INTERVAL 10 SECOND) >= '".date('H:m:s')."'";
+		$templateName = "";
+
+		$sql = "SELECT applianceID FROM schedules WHERE `scheduleDay` LIKE '%".date('l')."%' AND TIME(`timeStart`) <= '".date('H:i:s')."' AND DATE_ADD(TIME(`timeStart`), INTERVAL 10 SECOND) >= '".date('H:i:s')."'";
+
 
 		$result = mysqli_query($db, $sql);
 
-		if ($result) {
+		if (mysqli_num_rows($result) != 0 ) {
 
 			while ($data = mysqli_fetch_assoc($result)) {
-				$templateName = $data['templateName'];
+				$templateName = $data['applianceID'];
 			}
+
 		}
 		else {
 			$templateName = "none";
--- schedule.php
+++ schedule.php
@@ -1,20 +1,41 @@
+
+<h1>
+	<span id="currentTime">TIME</span>
+</h1>
+<h1>
+	<span id="templateName">TRY</span>
+</h1>
+
 <script type="text/javascript" src="./js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript">
 	
 	$(document).ready(function() {
 		
 		function checkSchedule() {
+			var dt = new Date($.now());
+			var hours = ((dt.getHours().toString().length === 1)? "0" + dt.getHours(): dt.getHours());
+			var minutes = ((dt.getMinutes().toString().length === 1)? "0" + dt.getMinutes(): dt.getMinutes());
+			var seconds = ((dt.getSeconds().toString().length === 1)? "0" + dt.getSeconds(): dt.getSeconds());
+			
+			$("#currentTime").text(hours + ":" + minutes + ":" + seconds);
 
 			$.post('./newChecker.php', function(data) {
+
 				var _data = JSON.parse(data);
 
+				// alert(_data);
+
 				if (_data == "none") {
-					alert(_data);
+					
+					$("#templateName").text(_data);
+
 				}
 				else {
 
 					$.post('./php/ActivateTemplate.php', {templateName: _data}, function(data) {
-						// TRY
+						
+						$("#templateName").text(_data);
+
 					});
 
 				}
@@ -22,7 +43,8 @@
 			});
 		}
 
-		setInterval(checkSchedule, 3000);
+		setInterval(checkSchedule, 1000);
 	});
 
 </script>
+
